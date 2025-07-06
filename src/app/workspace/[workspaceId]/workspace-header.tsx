bb7cba7c984f879ad78459dbcc54881bf4c3ea06
jjj <lynnjinjie@gmail.com>
2025-07-06 16:17:18 +0800

    feat(workspace): add invite code modal

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -10,6 +10,7 @@ import { DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu'
 import { ChevronDown, ListFilter, SquarePen } from 'lucide-react'
 import { useState } from 'react'
 import { Doc } from '../../../../convex/_generated/dataModel'
+import { InviteModal } from './invite-modal'
 import { PreferencesModal } from './preferences-modal'
 
 interface WorkspaceHeaderProps {
@@ -21,10 +22,17 @@ export const WorkspaceHeader = ({
   workspace,
   isAdmin,
 }: WorkspaceHeaderProps) => {
+  const [inviteOpen, setInviteOpen] = useState(false)
   const [preferencesopen, setPreferencesOpen] = useState(false)
 
   return (
     <>
+      <InviteModal
+        open={inviteOpen}
+        setOpen={setInviteOpen}
+        name={workspace.name}
+        joinCode={workspace.joinCode}
+      />
       <PreferencesModal
         open={preferencesopen}
         setOpen={setPreferencesOpen}
@@ -59,7 +67,7 @@ export const WorkspaceHeader = ({
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   className="cursor-pointer py-2"
-                  onClick={() => {}}
+                  onClick={() => setInviteOpen(true)}
                 >
                   Invite people to {workspace.name}
                 </DropdownMenuItem>
